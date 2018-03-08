e7f790690438e56331b392202665ba3775efdc4d
Mateusz <mateusz.adam.pawlik@gmail.com>
2018-03-08 23:23:33 +0100

    style, jquery

--- js/app.js
+++ js/app.js
@@ -1,10 +1,23 @@
 $( document ).ready(function() {
 
-    $('.fullpage').fullpage();
+    $('.fullpage').fullpage({
+    	navigation: true,
+    	navigationPosition: 'left'
+    });
 
     $('.sticky-menu #nav-icon1').click(function(){
 		$(this).toggleClass('open');
 		$( ".menu" ).toggleClass( "show" );
+	});
+
+	$('.start-element').hover(function(){
+
+			$(this).fadeOut(3000);
+
+	});
+
+	$('.download-cv').hover(function(){
+		$('.download-cv a').slideToggle('slow').stop(true, true);
 	});
 
 });
