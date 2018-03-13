640ca0a9424395501a4b3e8c43cd2d5421ba71a1
Mateusz <mateusz.adam.pawlik@gmail.com>
2018-03-14 00:42:26 +0100

    rwd menu

--- js/app.js
+++ js/app.js
@@ -2,12 +2,17 @@ $( document ).ready(function() {
 
     $('.fullpage').fullpage({
     	navigation: true,
-    	navigationPosition: 'left'
+    	navigationPosition: 'left',
+    	responsiveWidth: 1200,
+    	fixedElements: '.sticky-menu, .menu, .show'
     });
+
 
     $('.sticky-menu #nav-icon1').click(function(){
 		$(this).toggleClass('open');
-		$( ".menu" ).toggleClass( "show" );
+
+			$( ".menu" ).toggleClass( "show" );
+
 	});
 
 	$('.start-element').hover(function(){
